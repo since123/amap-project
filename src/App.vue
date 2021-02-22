1a382d8d2fb0678b9b15eb022f4b9def129cde6c
Alice Tan <alicetan@AlicedeMacBook-Pro.local>
2021-02-23 00:05:13 +0800

    vue3引入element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
   </div>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+export default {
+  mounted() {
+    let data = {
+      text: "ok",
+    };
+    window.addEventListener("unload", (event) => {
+      navigator.sendBeacon("http://localhost:3000/log", data);
+      // Cancel the event as stated by the standard.
+      event.preventDefault();
+      // Chrome requires returnValue to be set.
+      event.returnValue = "true";
+    });
+  },
+};
+</script>
 
 <style lang="stylus">
 #app
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
   color #2c3e50
   margin-top 60px
 </style>
